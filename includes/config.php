998236292e72b807120810ad922e05b81c569519
Richard Boon <99041876@mydavinci.nl>
2016-10-07 18:27:47 +0200

    First commit

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+define("HOST", "ricnet.nl");     // The host you want to connect to.
+define("USER", "fly_ricnet");    // The database username. 
+define("PASSWORD", "flyHELLO2211651@#$4216");    // The database password. 
+define("DATABASE", "fly_ricnet");    // The database name.
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+?>
