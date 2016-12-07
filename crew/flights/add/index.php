5a42afe945f60c96d28f4240ad0f23c15517e575
Richard Boon <99041876@mydavinci.nl>
2016-12-07 10:22:40 +0100

    Finished with form.

--- crew/flights/add/index.php
+++ crew/flights/add/index.php
@@ -1,20 +1,4 @@
 <?php
-/**
- * Copyright (C) 2013 peredur.net
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 include_once '../../../includes/db_connect.php';
 include_once '../../../includes/functions.php';
 
@@ -23,21 +7,41 @@ sec_session_start();
 $sql = "SELECT id, firstname, lastname FROM users";
 $result = $mysqli->query($sql);
 
-$result = $mysqli->query($sql);
-
-$allUsers;
-
 if ($result->num_rows > 0) {
 	while($row = $result->fetch_assoc()) {
 		if (isset($allUsers)) {
-			$allUsers += '<option value="'.$row["id"].'">'.$row["firstname"].' '.$row["lastname"].'</option>';
+			if ($row["id"] == $_SESSION['user_id']) {
+				$allUsersActive .= "<option selected value=\"" . $row["id"] . "\">" . $row["firstname"] . " " .$row["lastname"] . "</option>";
+			} else {
+				$allUsersActive .= "<option value=\"" . $row["id"] . "\">" . $row["firstname"] . " " .$row["lastname"] . "</option>";
+			}
+			$allUsers .= "<option value=\"" . $row["id"] . "\">" . $row["firstname"] . " " .$row["lastname"] . "</option>";
 		} else {
-			$allUsers = '<option value="'.$row["id"].'">'.$row["firstname"].' '.$row["lastname"].'</option>';
+			if ($row["id"] == $_SESSION['user_id']) {
+				echo "Test";
+				$allUsersActive = "<option selected value=\"" . $row["id"] . " selected\">" . $row["firstname"] . " " .$row["lastname"] . "</option>";
+			} else {
+				$allUsersActive = "<option value=\"" . $row["id"] . "\">" . $row["firstname"] . " " .$row["lastname"] . "</option>";
+			}
+			$allUsers = "<option value=\"" . $row["id"] . "\">" . $row["firstname"] . " " .$row["lastname"] . "</option>";
 		}
-		echo 'id: ' . $row["id"]. ', Name: ' . $row["firstname"]. ' ' . $row["lastname"]. '<br>';
 	}
 }
-echo $allUsers;
+$allUsersEmpty = "<option selected value></option>" . $allUsers;
+
+if (isset($_POST["airliner"], $_POST["flightnumber"], $_POST["from"], $_POST["to"], $_POST["aircraft"], $_POST["pic"], $_POST["firstOfficer"], $_POST["secondOfficer"], $_POST["preparedBy"], $_POST["atcRoute"], $_POST["fuel"])) {
+	$airliner = filter_input(INPUT_POST, 'airliner', FILTER_SANITIZE_STRING);
+	$flightNumber = filter_input(INPUT_POST, 'flightnumber', FILTER_SANITIZE_NUMBER_INT);
+	$fromAirport = filter_input(INPUT_POST, 'from', FILTER_SANITIZE_STRING);
+	$toAirport = filter_input(INPUT_POST, 'to', FILTER_SANITIZE_STRING);
+	$aircraft = filter_input(INPUT_POST, 'aircraft', FILTER_SANITIZE_STRING);
+	$pic = filter_input(INPUT_POST, 'pic', FILTER_SANITIZE_NUMBER_INT);
+	$firstOfficer = filter_input(INPUT_POST, 'firstOfficer', FILTER_SANITIZE_NUMBER_INT);
+	$secondOfficer = filter_input(INPUT_POST, 'secondOfficer', FILTER_SANITIZE_NUMBER_INT);
+	$preparedBy = filter_input(INPUT_POST, 'preparedBy', FILTER_SANITIZE_NUMBER_INT);
+	$atcRoute = filter_input(INPUT_POST, 'atcRoute', FILTER_SANITIZE_NUMBER_INT);
+	$fuel = filter_input(INPUT_POST, 'fuel', FILTER_SANITIZE_NUMBER_INT);
+};
 ?>
 <!DOCTYPE html>
 <html>
@@ -52,18 +56,19 @@ echo $allUsers;
 		<?php include '../../includes_pages/sideMenu.php'; ?>
 		<main>
 			<h2>Add flight</h2>
-			<form>
+			<form >
 				Airline: <input type="text" name="airliner"><br>
 				Flightnumber: <input type="number" name="flightnumber"><br>
 				From airport: <input type="text" name="from"><br>
 				To airport: <input type="text" name="to"><br>
 				Aircraft: <input type="text" name="aircraft"><br>
-				PIC: <select type="text" name="pic"><?php echo $allUsers; ?></select><br>
-				First officer: <select type="text" name="firstOfficer"><?php echo $allUsers; ?></select><br>
-				Second officer: <select type="text" name="secondOfficer"><?php echo $allUsers; ?></select><br>
-				Prepared by: <select type="text" name="preparedBy"><?php echo $allUsers; ?></select><br>
+				PIC: <select name="pic"><?php echo $allUsers; ?></select><br>
+				First officer: <select name="firstOfficer"><?php echo $allUsersEmpty; ?></select><br>
+				Second officer: <select name="secondOfficer"><?php echo $$allUsersEmpty; ?></select><br>
+				Prepared by: <select name="preparedBy"><?php echo $allUsersActive; ?></select><br>
 				ATC route: <textarea name="atcRoute"></textarea><br>
-				Release fuel: <input type="number" name="fuel">
+				Release fuel: <input type="number" name="fuel"><br>
+				<input type="submit" value="File flight">
 			</form>
 		</main>
 		<?php else : ?>
