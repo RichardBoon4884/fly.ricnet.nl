b23e4fd90df9d487e411f9fea5766a4ac5837342
Richard Boon <99041876@mydavinci.nl>
2017-01-17 10:55:40 +0100

    Imporved DB request

--- crew/index.php
+++ crew/index.php
@@ -20,17 +20,27 @@ include_once '../includes/functions.php';
 
 sec_session_start();
 
-$sql = "SELECT flights.id, airlines.icao AS airline, flights.flightnumber, airports.icao AS fromAirport, flights.toAirport, flights.aircraft, flights.pic, flights.firstofficer, flights.secondofficer, flights.preparedby, flights.atcroute, flights.releasefuel FROM flights INNER JOIN airlines ON flights.airline = airlines.id INNER JOIN airports ON flights.fromAirport = airports.id ";
+$sql = "SELECT flights.id, airlines.icao AS airline, flights.flightnumber, airports1.icao AS fromAirport, airports2.icao AS toAirport, flights.aircraft, flights.pic, usersPic.username as picUsername, flights.firstofficer, usersFirstofficer.username as firstofficerUsername, flights.secondofficer, usersSecondofficer.username as secondofficerUsername, flights.preparedby, usersPreparedby.username as preparedbyUsername, flights.atcroute, flights.releasefuel FROM flights
+INNER JOIN airlines ON flights.airline = airlines.id
+INNER JOIN airports airports1 ON flights.fromAirport = airports1.id
+INNER JOIN airports airports2 ON flights.toAirport = airports2.id
+INNER JOIN users usersPic ON flights.pic = usersPic.id
+LEFT JOIN users usersFirstofficer ON flights.firstofficer = usersFirstofficer.id
+LEFT JOIN users usersSecondofficer ON flights.secondofficer = usersSecondofficer.id
+LEFT JOIN users usersPreparedby ON flights.preparedby = usersPreparedby.id";
 $result = $mysqli->query($sql);
 
 if ($result->num_rows > 0) {
 	while($row = $result->fetch_assoc()) {
-
-
-		if (isset($flights)) {
-			$flights .= "<a href=\"?flight=" . $row["id"] . "\"><li class=\"box\" id=\"" . $row["id"] . "\"><div class=\"chartName\">" . $row["airline"] . " " . $row["flightnumber"] . "</div><div  class=\"chartDescription\">" . $row["fromAirport"] . " > ??</div></li></a>";
+		if ($row["preparedbyUsername"] != "") {
+			$preparedby = "<div>Prepared by " . $row["preparedbyUsername"] . ".</div>";
 		} else {
-			$flights = "<a href=\"?flight=" . $row["id"] . "\"><li class=\"box\" id=\"" . $row["id"] . "\"><div class=\"chartName\">" . $row["airline"] . " " . $row["flightnumber"] . "</div><div  class=\"chartDescription\">" . $row["fromAirport"] . " > ??</div></li></a>";
+			$preparedby = "";
+		}
+		if (isset($flights)) {
+			$flights .= "<a href=\"?flight=" . $row["id"] . "\"><li class=\"box\" id=\"" . $row["id"] . "\"><div class=\"chartName\">" . $row["airline"] . " " . $row["flightnumber"] . "</div><div  class=\"chartDescription\">" . $row["fromAirport"] . " > " . $row["toAirport"] . "</div>" . $preparedby . "</li></a>";
+		} else {
+			$flights = "<a href=\"?flight=" . $row["id"] . "\"><li class=\"box\" id=\"" . $row["id"] . "\"><div class=\"chartName\">" . $row["airline"] . " " . $row["flightnumber"] . "</div><div  class=\"chartDescription\">" . $row["fromAirport"] . " > " . $row["toAirport"] . "</div></li></a>";
 		}
 	}
 }
