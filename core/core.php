67c150ab21718090181b74956739733ff0997e67
Richard Boon <99041876@mydavinci.nl>
2017-03-09 21:43:46 +0100

    PDO errors and warnings

--- core/core.php
+++ core/core.php
@@ -1,7 +1,7 @@
 <?php
 
 function openDb() {
-	$db = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_DATABASE, DB_USER, DB_PASSWORD);
+	$db = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_DATABASE, DB_USER, DB_PASSWORD, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 
 	return $db;
 }
