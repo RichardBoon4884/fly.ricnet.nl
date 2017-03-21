9c341fe5ad02a748b3aa111fd9c8391ef381acc9
Richard Boon <99041876@mydavinci.nl>
2017-03-21 14:34:57 +0100

    Working map

--- model/FlightModel.php
+++ model/FlightModel.php
@@ -25,16 +25,26 @@ class FlightModel {
     public function getFlight($id) {
         $db = openDb();
 
-        $sth = $db->prepare("SELECT flights.id, airlines.icao AS airline, flights.flightnumber, flights.fromAirport, airports1.icao AS fromAirportIcao, flights.toAirport, airports2.icao AS toAirportIcao, flights.aircraft, flights.pic, usersPic.username as picUsername, flights.firstofficer, usersFirstofficer.username as firstofficerUsername, flights.secondofficer, usersSecondofficer.username as secondofficerUsername, flights.preparedby, usersPreparedby.username as preparedbyUsername, flights.atcroute, flights.releasefuel FROM flights
-			INNER JOIN airlines ON flights.airline = airlines.id
-			INNER JOIN airports airports1 ON flights.fromAirport = airports1.id
-			INNER JOIN airports airports2 ON flights.toAirport = airports2.id
-			INNER JOIN users usersPic ON flights.pic = usersPic.id
-			LEFT JOIN users usersFirstofficer ON flights.firstofficer = usersFirstofficer.id
-			LEFT JOIN users usersSecondofficer ON flights.secondofficer = usersSecondofficer.id
-			LEFT JOIN users usersPreparedby ON flights.preparedby = usersPreparedby.id
-			WHERE flights.id = :requestedFlight
-			LIMIT 1");
+        $sth = $db->prepare("SELECT flights.id, airlines.icao AS airline, flights.flightnumber, flights.fromAirport, airports1.icao AS fromAirportIcao, airports1Name.name as fromAirportName ,airports1Municipality.municipality as fromAirportMunicipality, airports1CountryCode.iso_country as fromAirportCountryCode, airports1Latitude.latitude AS fromAirportLatitude, airports1Longitude.longitude AS fromAirportLongitude, flights.toAirport, airports2.icao AS toAirportIcao, airports2Name.name as toAirportName, airports2Municipality.municipality as toAirportMunicipality, airports2CountryCode.iso_country as toAirportCountryCode, airports2Latitude.latitude as toAirportLatitude, airports2Longitude.longitude as toAirportLongitude, flights.aircraft, flights.pic, usersPic.username as picUsername, flights.firstofficer, usersFirstofficer.username as firstofficerUsername, flights.secondofficer, usersSecondofficer.username as secondofficerUsername, flights.preparedby, usersPreparedby.username as preparedbyUsername, flights.atcroute, flights.releasefuel FROM flights
+            INNER JOIN airlines ON flights.airline = airlines.id
+            INNER JOIN airports airports1 ON flights.fromAirport = airports1.id
+            INNER JOIN airports airports1Name ON flights.fromAirport = airports1Name.id
+            INNER JOIN airports airports1Municipality ON flights.fromAirport = airports1Municipality.id
+            INNER JOIN airports airports1CountryCode ON flights.fromAirport = airports1CountryCode.id
+            INNER JOIN airports airports1Latitude ON flights.fromAirport = airports1Latitude.id
+            INNER JOIN airports airports1Longitude ON flights.fromAirport = airports1Longitude.id
+            INNER JOIN airports airports2 ON flights.toAirport = airports2.id
+            INNER JOIN airports airports2Name ON flights.toAirport = airports2Name.id
+            INNER JOIN airports airports2Municipality ON flights.toAirport = airports2Municipality.id
+            INNER JOIN airports airports2CountryCode ON flights.toAirport = airports2CountryCode.id
+            INNER JOIN airports airports2Latitude ON flights.toAirport = airports2Latitude.id
+            INNER JOIN airports airports2Longitude ON flights.toAirport = airports2Longitude.id
+            INNER JOIN users usersPic ON flights.pic = usersPic.id
+            LEFT JOIN users usersFirstofficer ON flights.firstofficer = usersFirstofficer.id
+            LEFT JOIN users usersSecondofficer ON flights.secondofficer = usersSecondofficer.id
+            LEFT JOIN users usersPreparedby ON flights.preparedby = usersPreparedby.id
+            WHERE flights.id = :requestedFlight
+            LIMIT 1");
         $sth->bindParam(':requestedFlight', $id, PDO::PARAM_STR);
 
         $sth->execute();
--- view/viewFlights.php
+++ view/viewFlights.php
@@ -13,6 +13,28 @@
                 <div id="mapid" style="height: 500px;"></div>
 				<h2><?php echo $flightInfo['airline'] . " " . $flightInfo['flightnumber']; ?></h2>
 				<div><?php echo $flightInfo['fromAirportIcao'] . " > " . $flightInfo['toAirportIcao']; ?></div>
-                <script>var mymap = L.map('mapid').setView([51.505, -0.09], 13);</script>
+                <script>
+
+var mymap = L.map('mapid').setView([<?php print $flightInfo["fromAirportLatitude"]; ?>, <?php print $flightInfo["fromAirportLongitude"]; ?>], 13);
+var marker = L.marker([<?php print $flightInfo["fromAirportLatitude"]; ?>, <?php print $flightInfo["fromAirportLongitude"]; ?>]).addTo(mymap);
+L.tileLayer('https://api.mapbox.com/styles/v1/richardboon4884/cj0jbos6a00j42spdfiduad4n/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicmljaGFyZGJvb240ODg0IiwiYSI6ImNqMGpiOWpqbTAwMW0zM3BtdWk2Z3ptYTYifQ.LtjlK_yXDQLgtutFAtqt2w', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'cj0jbcqtj00162ppkhiy0tsl4',
+    accessToken: 'pk.eyJ1IjoicmljaGFyZGJvb240ODg0IiwiYSI6ImNqMGpiOWpqbTAwMW0zM3BtdWk2Z3ptYTYifQ.LtjlK_yXDQLgtutFAtqt2w'
+}).addTo(mymap)
+
+marker.bindPopup("<?php print $flightInfo["fromAirportName"]; ?><br><?php print $flightInfo["fromAirportMunicipality"]; ?>, <?php print $flightInfo["fromAirportCountryCode"]; ?>");
+
+function midPoint(lat1,lon1,lat2,lon2)
+{
+    lat3 = (lat1 + lat2);
+    lat3 = lat3/2;
+
+    lon3 = (lon1 + lon2);
+    lon3 = lon3/2;
+
+    return (lat3 + ", " + lon3);
+}</script>
 			</aside>
 <?php endif; ?>
