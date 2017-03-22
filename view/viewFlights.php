cb988c369140680a506dc50e5717fe58aa7e3de6
Richard Boon <99041876@mydavinci.nl>
2017-03-22 09:32:37 +0100

    Working map

--- view/viewFlights.php
+++ view/viewFlights.php
@@ -16,7 +16,6 @@
                 <script>
 
 var mymap = L.map('mapid').setView([<?php print $flightInfo["fromAirportLatitude"]; ?>, <?php print $flightInfo["fromAirportLongitude"]; ?>], 13);
-var marker = L.marker([<?php print $flightInfo["fromAirportLatitude"]; ?>, <?php print $flightInfo["fromAirportLongitude"]; ?>]).addTo(mymap);
 L.tileLayer('https://api.mapbox.com/styles/v1/richardboon4884/cj0jbos6a00j42spdfiduad4n/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicmljaGFyZGJvb240ODg0IiwiYSI6ImNqMGpiOWpqbTAwMW0zM3BtdWk2Z3ptYTYifQ.LtjlK_yXDQLgtutFAtqt2w', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
@@ -24,17 +23,23 @@ L.tileLayer('https://api.mapbox.com/styles/v1/richardboon4884/cj0jbos6a00j42spdf
     accessToken: 'pk.eyJ1IjoicmljaGFyZGJvb240ODg0IiwiYSI6ImNqMGpiOWpqbTAwMW0zM3BtdWk2Z3ptYTYifQ.LtjlK_yXDQLgtutFAtqt2w'
 }).addTo(mymap)
 
-marker.bindPopup("<?php print $flightInfo["fromAirportName"]; ?><br><?php print $flightInfo["fromAirportMunicipality"]; ?>, <?php print $flightInfo["fromAirportCountryCode"]; ?>");
+var fromAirport = L.marker([<?php print $flightInfo["fromAirportLatitude"]; ?>, <?php print $flightInfo["fromAirportLongitude"]; ?>]).addTo(mymap);
+fromAirport.bindPopup("<?php print $flightInfo["fromAirportName"]; ?><br><?php print $flightInfo["fromAirportMunicipality"]; ?>, <?php print $flightInfo["fromAirportCountryCode"]; ?>");
 
-function midPoint(lat1,lon1,lat2,lon2)
-{
-    lat3 = (lat1 + lat2);
-    lat3 = lat3/2;
+var toAirport = L.marker([<?php print $flightInfo["toAirportLatitude"]; ?>, <?php print $flightInfo["toAirportLongitude"]; ?>]).addTo(mymap);
+toAirport.bindPopup("<?php print $flightInfo["toAirportName"]; ?><br><?php print $flightInfo["toAirportMunicipality"]; ?>, <?php print $flightInfo["toAirportCountryCode"]; ?>");
 
-    lon3 = (lon1 + lon2);
-    lon3 = lon3/2;
+var line = L.polygon([
+    [<?php print $flightInfo["fromAirportLatitude"]; ?>, <?php print $flightInfo["fromAirportLongitude"]; ?>],
+    [<?php print $flightInfo["toAirportLatitude"]; ?>, <?php print $flightInfo["toAirportLongitude"]; ?>]
+]).addTo(mymap);
+line.setStyle({
+    weight: 3.5,
+    color: '#000'
+});
 
-    return (lat3 + ", " + lon3);
-}</script>
+var route = new L.featureGroup([fromAirport, toAirport, line]);
+
+mymap.fitBounds(route.getBounds());</script>
 			</aside>
 <?php endif; ?>
